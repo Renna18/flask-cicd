b0ecca8d7fc036bca44142446e099cab44e212b5
Renna18 <remartinez@my.cspc.edu.ph>
2026-05-01 17:00:11 +0800

    Fix broken test

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ from app.main import app
 def test_home():
     client = app.test_client()
     response = client.get("/")
-    assert response.status_code == 999
+    assert response.status_code == 200
 
 def test_health():
     client = app.test_client()
